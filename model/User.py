9a22e96bab67d47b696ebfb868444349e3765bd4
tgiker <tgiker@gmail.com>
2023-12-04 12:41:00 +0100

    aldaketak User

--- model/User.py
+++ model/User.py
@@ -13,29 +13,76 @@ class Session:
 		return f"{self.hash} ({self.time})"
 
 class User:
-	def __init__(self, id, username, email):
-		self.id = id
+	def __init__(self, username, argazkia, administratzaileaDa):
 		self.username = username
-		self.email = email
+		self.argazkia = argazkia
+		self.administratzaileaDa = administratzaileaDa
 
 	def __str__(self):
-		return f"{self.username} ({self.email})"
+		return f"{self.username}"
 
 	def new_session(self):
 		now = float(datetime.datetime.now().time().strftime("%Y%m%d%H%M%S.%f"))
-		session_hash = hash_password(str(self.id)+str(now))
-		db.insert("INSERT INTO Session VALUES (?, ?, ?)", (session_hash, self.id, now))
+		session_hash = hash_password(str(self.username)+str(now))
+		db.insert("INSERT INTO Session VALUES (?, ?, ?)", (session_hash, self.username, now))
 		return Session(session_hash, now)
 
 	def validate_session(self, session_hash):
-		s = db.select("SELECT * from Session WHERE user_id = ? AND session_hash = ?", (self.id, session_hash))
+		s = db.select("SELECT * from Session WHERE user_id = ? AND session_hash = ?", (self.username, session_hash))
 		if len(s) > 0:
 			now = float(datetime.datetime.now().strftime("%Y%m%d%H%M%S.%f"))
-			session_hash_new = hash_password(str(self.id) + str(now))
-			db.update("UPDATE Session SET session_hash = ?, last_login=? WHERE session_hash = ? and user_id = ?", (session_hash_new, now, session_hash, self.id))
+			session_hash_new = hash_password(str(self.username) + str(now))
+			db.update("UPDATE Session SET session_hash = ?, last_login=? WHERE session_hash = ? and user_id = ?", (session_hash_new, now, session_hash, self.username))
 			return Session(session_hash_new, now)
 		else:
 			return None
 
 	def delete_session(self, session_hash):
-		db.delete("DELETE FROM Session WHERE session_hash = ? AND user_id = ?", (session_hash, self.id))
+		db.delete("DELETE FROM Session WHERE session_hash = ? AND user_id = ?", (session_hash, self.username))
+
+	# FOROAK
+
+	def foroaBerriaSortu(self, fIzena, eIzena, deskribapena):
+		# HAY QUE HACERLO
+		return
+
+	# LAGUNAK
+
+	def lagunListaSortu(self):
+		# HAY QUE HACERLO
+		return
+	def lagunEskaeraListaLortu(self):
+		# HAY QUE HACERLO
+		return
+	def lagunEskaeraKudeatu(self, erantzuna):
+		# HAY QUE HACERLO
+		return
+	def lagunEskaeraBidali(self, eIzena):
+		# HAY QUE HACERLO
+		return
+
+	# ERRESERBAK
+
+	def erreserbaListaLortu(self):
+		# HAY QUE HACERLO
+		return
+
+	# ERRESEINAK
+
+	def erreseinaListaLortu(self):
+		# HAY QUE HACERLO
+		return
+
+	# ADMINISTRATZAILE FUNTZIOAK
+
+	def liburuBerriaGehitu(self, portada, izenburua, urtea, idazlea, sinopsia, PDF):
+		# HAY QUE HACERLO
+		return
+
+	def erabiltzaileBErriaSortu(self, eIzena, izenAbizenak, pasahitza, nan, tel, pElek, helb, argazkia, administratzaileaDa):
+		# HAY QUE HACERLO
+		return
+
+	def erabiltzaileaEzabatu(self, eIzena):
+		# HAY QUE HACERLO
+		return
