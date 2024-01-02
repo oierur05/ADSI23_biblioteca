0ed15f13e30218b8b8efcdc39085b6b0e22f72fb
irune <irunepalacios2003@gmail.com>
2024-01-02 17:24:32 +0100

    gitignore

--- tests/test_administratzailea.py
+++ tests/test_administratzailea.py
@@ -1,13 +1,27 @@
+from bs4 import BeautifulSoup
+
 from . import BaseTestClass
 
 class TestAdministratzailea(BaseTestClass):
 
     def test_aukerak_ikusi(self):
-        # testak: okerra:
-        #           ezin ditu aukerak ikusi
-        #         zuzena:
-        #           aukerak ikus ditzake
-		self.assertTrue(False)
+        # testak:
+        #           1. erabiltzailea administratzailea da
+        #           2. erabiltzailea ez da administratzailea
+        # [1]
+        self.sartu('juanbelio', 'juan')
+        res = self.client.get('/perfila')
+        self.assertEqual(200, res.status_code)
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(2, len(page.find_all('div', class_='card-body')))
+        self.irten()
+        # [2]
+        self.sartu('numen_0', 'calvo')
+        res = self.client.get('/perfila')
+        self.assertEqual(200, res.status_code)
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual(1, len(page.find_all('div', class_='card-body')))
+        self.irten()
 
     def test_erabiltzaile_berria_sortu(self):
         # testak: okerra:
@@ -15,11 +29,16 @@ class TestAdministratzailea(BaseTestClass):
         #           erabiltzaile izen errepikatua duen erabiltzaile bat sortzen da
         #         zuzena:
         #           erabiltzaile izen bakarra duen erabiltzaile berri bat sortzen da
-		self.assertTrue(False)
+        self.sartu('juanbelio', 'juan')
+        res = self.client.get('/erabSortu')
+        self.assertEqual(200, res.status_code)
+        page = BeautifulSoup(res.data, features="html.parser")
+
+
 
     def test_erabiltzailea_ezabatu(self):
         # testak: okerra:
         #           ez da erabiltzailea ezabatu
         #         zuzena:
         #           erabiltzailea ezabatu da
-		self.assertTrue(False)
+        self.assertTrue(False)
