5f8e04187065d004cee5d589b4e728b88c6bc2a0
tgiker <tgiker@gmail.com>
2024-01-03 16:11:17 +0100

    cosas del html y mas

--- model/load_data.py
+++ model/load_data.py
@@ -29,7 +29,7 @@ cur.execute("""
 	)
 """)
 
-#cur.execute("""DELETE FROM Erabiltzailea WHERE administratzaileada='True'""")
+#cur.execute("""DELETE FROM Liburua WHERE liburuid > 5 and liburuid < 17""")
 #con.commit()
 
 cur.execute("""
